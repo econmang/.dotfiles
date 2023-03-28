e6d77186e10aad22c38a25aa6e70cf52840c8d7c
Evan Conley <econmang@gmail.com>
2023-03-27 21:33:22 -0700

    Adding configs for git, alacritty, i3, nvim. Also migrating .zshrc

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env zsh
 
+# Set the folders to stow
+if [[ -z $STOW_FOLDERS ]]; then
+    STOW_FOLDERS="alacritty,i3,nvim,zsh"
+fi
+
+if [[ -z $DOTFILES ]]; then
+    DOTFILES=$HOME/.dotfiles
+fi
+
+STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES #$DOTFILES/install
+
+echo "stowing folders: $STOW_FOLDERS"
+echo "dotfiles folder: $DOTFILES"
+
 pushd $DOTIFLES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
     echo "stow $folder"
-    stow -D $folder
+    #stow -D $folder
     stow $folder
 done
 popd
