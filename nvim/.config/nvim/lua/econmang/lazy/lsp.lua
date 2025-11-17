8a24c1f8d2e0347894c558fbe11a7a0e0a90261e
Evan Conley <econmang@user.noreply.github.com>
2025-11-16 19:37:53 -0800

    updated libs to load lazydev

--- nvim/.config/nvim/lua/econmang/lazy/lsp.lua
+++ nvim/.config/nvim/lua/econmang/lazy/lsp.lua
@@ -8,6 +8,8 @@ return {
 			library = {
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+                { path = "lazy.nvim", words = { "LazyVim" } },
 			},
 		},
 	},
